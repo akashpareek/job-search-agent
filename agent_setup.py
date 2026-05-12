546f5a2621ddb962ab5d8ef065350f0208fa740f
akashpareek <pareekakash3@gmail.com>
2026-05-13 01:36:13 +0530

    memory removed

--- agent_setup.py
+++ agent_setup.py
@@ -4,9 +4,7 @@ from langchain.agents import AgentType
 from langchain_groq import ChatGroq
 
 from agent_tools import search_jobs, experience_filter
-from langchain.memory import ConversationBufferMemory
 
-memory = ConversationBufferMemory()
 
 llm = ChatGroq(
     model_name="llama-3.1-8b-instant",
@@ -19,7 +17,6 @@ tools = [search_jobs, experience_filter]
 agent = initialize_agent(
     tools=tools,
     llm=llm,
-    memory=memory,
     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     verbose=False,
     max_iterations=3,
